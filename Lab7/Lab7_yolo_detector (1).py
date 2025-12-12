835109f55462292e74c0236656a5257c5398dddf
mwauila20 <90110442+mwauila20@users.noreply.github.com>
2025-12-12 07:04:44 +0300

    Add files via upload

---
+++ Lab7/Lab7_yolo_detector (1).py
@@ -0,0 +1,19 @@
+﻿from ultralytics import YOLO
+
+# Загрузка предобученной модели YOLOv5
+model = YOLO("yolov8s.pt")
+
+# Путь к YAML файлу, который содержит данные для обучения
+data_yaml = "data1.yaml"
+
+# Обучение модели
+model.train(data=data_yaml, epochs=50, batch=16, imgsz=640)
+
+# Сохранение обученной модели
+model.save("trained_model.pt")
+
+# Тестирование модели на новом изображении
+#results = model("path/to/test/image.jpg")
+
+# Показ результатов
+#results.show()
